beafd9828e9a3a42f67bf56c27253b500fd704ab
Vitaly Zubkov <zubkov.v.o@gmail.com>
2019-03-13 17:10:54 +0400

    created a project with basic class and pom.xml configuration file

---
+++ src/test/java/com/java/training/app/MainTest.java
@@ -0,0 +1,13 @@
+package com.java.training.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MainTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
